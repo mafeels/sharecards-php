c7b89baa88daf5f159c5c250ee8036961ad66877
mattalvesartist <mathew.eu@live.com>
2018-07-03 19:13:15 -0300

    erro maldito

--- web/public_html/dashFlash.php
+++ web/public_html/dashFlash.php
@@ -143,6 +143,7 @@
                                             include_once("connection_factory.php");
 
                                             $query = "SELECT * from flashcard WHERE codigo_usuario='".$_SESSION["codigo_usuario"]."' ";
+
                                             $result = mysqli_query($conexao,$query);
                                             $num_results = mysqli_num_rows($result);
 
@@ -150,7 +151,7 @@
                                             for ($i=0; $i<$num_results; $i++)
                                             {
                                                 $row = mysqli_fetch_array($result);
-                                                if($row[0]==$_SESSION["codigo_flashcard"] && $row[1]==$_SESSION["codigo_usuario"]){
+                                                if($row[0]==$_GET["id"] && $row[1]==$_SESSION["codigo_usuario"]){
                                                     echo $row[4];
                                                  }
                                             }
